e18a9130f52a1607c6ccceb030d541e12432271a
FinBots <finbots@opsbots.org>
2021-09-03 17:24:48 -0500

    add ccxt markets fetch

--- c-coinbase.py
+++ c-coinbase.py
@@ -5,7 +5,23 @@ import pandas as pd
 # print(ccxt.exchanges)
 
 exchange = ccxt.coinbasepro()
-bars = exchange.fetch_ohlcv('ETH/USDT', timeframe='1m', limit=30)
+print(exchange)
+
+markets = exchange.load_markets()
+
+for market in markets:
+  print(market)
+
+# exchange_id = 'coinbasepro'
+# exchange_class = getattr(ccxt, exchange_id)
+# exchange = {
+#   'apiKey' = 'YOUR_API_KEY'
+#   'secret' = 'YOUR_SECRET'
+#   'TIMEOUT' = 30000
+#   'enableRateLimit' = True
+# }
+
+bars = exchange.fetch_ohlcv('ADA/USD', timeframe='1m', limit=30)
 df = pd.DataFrame(bars, columns=['timestamp','open','high','low','close','volume'])
 df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
 
