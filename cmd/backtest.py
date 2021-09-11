022b066d6e119444bcaa67ecb38e5292a8e60ca6
FinBots <finbots@opsbots.org>
2021-09-11 14:42:53 -0500

    pass through parsed args to external script

--- cmd/backtest.py
+++ cmd/backtest.py
@@ -12,13 +12,34 @@ from pytrader.log import logger
 
 def argparser(subparser):
   parser = subparser.add_parser('backtest')
+  parser.add_argument('-i', '--interval', default=[], help="data granularity for test" )
   parser.add_argument('-s', '--strategy', default='openbreakout', help="the strategy" )
   parser.add_argument('-t', '--ticker', default=[], help="the ticker symbol" )
   return parser
 
 def main(args):
-  logger.debug('pytrader backtest')
+  logger.info('pytrader backtest')
   logger.debug(args)
-  os.system(f'python backtest/{args.strategy}.py')
+
+  args_str=""
+  for arg in vars(args):
+
+    if arg == 'subcommand':
+      continue
+
+    if arg == 'strategy':
+      continue
+
+    if arg == 'verbose':
+      if getattr(args, arg) == True:
+        args_str = args_str + f"--{arg}"
+      else:
+        continue
+
+    args_str = args_str + f"--{arg} {getattr(args, arg)} "
+
+
+  cmd = f'python backtest/{args.strategy}.py {args_str}'
+  os.system(cmd)
 
 
